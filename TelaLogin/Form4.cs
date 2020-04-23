0e367212d2e23dc5c7a21f5cd21373dcca2d9146
Jonathan cesar <jonathan_cesar2015@hotmail.com>
2020-04-23 19:57:30 -0300

    agora vai denner

---
+++ TelaLogin/Form4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelaLogin
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnEntrada_Click(object sender, EventArgs e)
+        {
+            var Form3 = new Form3();
+            this.Hide(); //vai “esconder” o formulário atual
+            Form3.ShowDialog();
+        }
+    }
+}
